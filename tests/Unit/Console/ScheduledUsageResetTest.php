b048af0d1423419bcebe71377fc6b6c964f52982
Selase Kwawu <hiselase@gmail.com>
2026-09-05 11:12:01 +0000

    Remove unused Artisan import from ScheduledUsageResetTest
    Was failing vendor/bin/pint --test.

--- tests/Unit/Console/ScheduledUsageResetTest.php
+++ tests/Unit/Console/ScheduledUsageResetTest.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Illuminate\Console\Scheduling\Schedule;
-use Illuminate\Support\Facades\Artisan;
 
 test('the schedule resets event_registrations and email_credits usage monthly', function () {
     $schedule = app(Schedule::class);
